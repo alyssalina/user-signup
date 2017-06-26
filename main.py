cc399056f0c7d481e6a05c6574c0fef380dee06d
Alyssa Askew <alyssa.d.askew@gmail.com>
2017-06-25 22:31:39 -0700

    Fixed a stupid bug

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
-@app.route('/welcome_page', methods=['POST'])
-def welcome():
-    username = request.form['username']
-    return render_template('welcome_page.html', username=username)
-#this might break shit - review boolean values
-def field_filled(field_value):
-    return field_value !=''
 
+def field_empty(field_value):
+    return field_value == ''
+
+@app.route('/', methods=['POST'])
 def validate_entries():
     username = request.form['username']
     password = request.form['password']
@@ -29,11 +26,8 @@ def validate_entries():
     password2_error = ''
     email_error = ''
 
-#check for username errors
-
-    if not field_filled(username):
+    if field_empty(username):
         username_error = 'Please fill in a username'
-        username = ''
     else:
         if len(username)< 3 or len(username)>20:
             username_error = 'Username must be between 3-20 characters'
@@ -42,12 +36,12 @@ def validate_entries():
             username_error = "Sorry, you cannot have spaces in your username. All other characters are fine!"
             
 
-    if not field_filled(password):
+    if field_empty(password):
         password_error = "Please create a password"
         password = ''
 
-    if not field_filled(password2):
-        password_error = "Please verify your password"
+    if field_empty(password2):
+        password2_error = "Please verify your password"
         password2 = ''
 
     if password != password2:
@@ -62,10 +56,18 @@ def validate_entries():
 
     #check if there is anything in the error strings
 
-    if not username_error and not password_error and not password2_error and not email_error:
+    if not password_error and not password2_error and not username_error and not email_error:
         return redirect('welcome_page?username={0}'.format(username))
     else:
-        return render_template('index.html',username_error=username_error, password_error=password_error,password2_error=password2_error,email_error=email_error)
+        return render_template('index.html',username_error=username_error, 
+        password_error=password_error,
+        password2_error=password2_error,email_error=email_error)
+
+@app.route('/welcome_page', methods=['POST'])
+def welcome():
+    username = request.form['username']
+    return render_template('welcome_page.html', username=username)
+
         
 
 app.run()
